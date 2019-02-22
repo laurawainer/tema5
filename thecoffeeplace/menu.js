fcdd5ee66e1726701e3f3dfe56e09a83bdaa5c53
laurawainer <laur172n@stud.kea.dk>
2019-02-22 16:32:54 +0100

    første menu beskrivelser js fil

---
+++ thecoffeeplace/menu.js
@@ -0,0 +1,185 @@
+[
+    {
+        "navn": "Flowerpower french toast",
+        "billede": "breakfast1",
+        "kort": "A delicious french toast, served with fresh fruit and edible flowers.",
+        "lang": "",
+        "pris": "109",
+        "kategori": "breakfast"
+    }, {
+        "navn": "Chocopops waffles",
+        "billede": "breakfast2",
+        "kort": "Waffles made with chocopops, served wih fresh fruit. ",
+        "lang": "",
+        "pris": "119",
+        "kategori": "breakfast"
+    }, {
+        "navn": "Sourdough and scramble eggs",
+        "billede": "breakfast3",
+        "kort": "Fresh sourdogh and the best scramble eggs you have ever tasted!",
+        "lang": "",
+        "pris": "109",
+        "kategori": "breakfast"
+    }, {
+        "navn": "Crossant benedict",
+        "billede": "breakfast4",
+        "kort": "The classic eggs benedict has been taken to new height in this delicious version.",
+        "lang": "",
+        "pris": "149",
+        "kategori": "breakfast"
+    }, {
+        "navn": "fruity fruit salad",
+        "billede": "breakfast5",
+        "kort": "A combination of all the best in season fruit, collected from our local farmers.",
+        "lang": "",
+        "pris": "109",
+        "kategori": "breakfast"
+    }, {
+        "navn": "eggs and salmon toast",
+        "billede": "breakfast6",
+        "kort": "Norwegian salmon with our beautiful freshly baked sourdough, a combination you will not regret.",
+        "lang": "",
+        "pris": "129",
+        "kategori": "breakfast"
+    }, {
+        "navn": "All day fruit salad",
+        "billede": "lunch1",
+        "kort": "Our beautiful fruit salad, with a few delicious vegstables hiding underneath is the perfect lunch.",
+        "lang": "",
+        "pris": "99",
+        "kategori": "lunch"
+    }, {
+        "navn": "Chicken Tikka Masala",
+        "billede": "lunch2",
+        "kort": "Kyllingebryst tilberedt i tandooriovn serveret i masala sovs.",
+        "lang": "",
+        "pris": "115",
+        "kategori": "lunch"
+    }, {
+        "navn": "Shahjani Curry",
+        "billede": "lunch3",
+        "kort": "Kokkens egen opskrift på kylling i karry.",
+        "lang": "",
+        "pris": "99",
+        "kategori": "lunch"
+    }, {
+        "navn": "Butter Chicken",
+        "billede": "lunch4",
+        "kort": "Kylling smørstegt i tandooriovn tilberedt med orientalske krydderier samt tomater.",
+        "lang": "",
+        "pris": "110",
+        "kategori": "lunch"
+    }, {
+        "navn": "Nawabi Murgh",
+        "billede": "lunch5",
+        "kort": "Muglai specialitet, udbenet kylling i krydret karry serveret med hakket kød og nødder.",
+        "lang": "",
+        "pris": "109",
+        "kategori": "lunch"
+    }, {
+        "navn": "Saag Chicken",
+        "billede": "lunch6",
+        "kort": "Udbenede kyllingestykker tilberedt i karry med spinat.",
+        "lang": "",
+        "pris": "129",
+        "kategori": "lunch"
+    }, {
+        "navn": "Chicken Biryani",
+        "billede": "chicken-tikka-biryani",
+        "kort": "Ris blandet med kylling á la mughali.",
+        "lang": "",
+        "pris": "99",
+        "kategori": "kylling"
+    }, {
+        "navn": "Champ Tandoori",
+        "billede": "tandoori-champ",
+        "kort": "Specialtilberedte indiske lammekoteletter.",
+        "lang": "",
+        "pris": "135",
+        "kategori": "lam"
+    }, {
+        "navn": "Rogan Josh",
+        "billede": "rogan-josh",
+        "kort": "Lammekød i kraftig krydret karry.",
+        "lang": "",
+        "pris": "105",
+        "kategori": "lam"
+    }, {
+        "navn": "Shahi Korma Lahori",
+        "billede": "shahi-korma-lahori",
+        "kort": "Små stykker af udbenet lammekød i mild karry.",
+        "lang": "",
+        "pris": "105",
+        "kategori": "lam"
+    }, {
+        "navn": "Mutten Do Piaza",
+        "billede": "mutten-do-piaza",
+        "kort": "Kokkens egen opskrift på krydret lammekød.",
+        "lang": "",
+        "pris": "105",
+        "kategori": "lam"
+    }, {
+        "navn": "Keema Matter",
+        "billede": "keema-matter",
+        "kort": "Hakket kød med ærter.",
+        "lang": "",
+        "pris": "105",
+        "kategori": "lam"
+    }, {
+        "navn": "Raita",
+        "billede": "raita",
+        "kort": "Agurk og tomat blandet i kærnet yoghurt.",
+        "lang": "",
+        "pris": "35",
+        "kategori": "vegetar"
+    }, {
+        "navn": "Daal",
+        "billede": "daal",
+        "kort": "Kokkens specialitet. Linser stegt i smør.",
+        "lang": "",
+        "pris": "85",
+        "kategori": "vegetar"
+    }, {
+        "navn": "Paneer Makhani",
+        "billede": "paneer-makhani",
+        "kort": "Frisk hytteost i mild flødesovs.",
+        "lang": "",
+        "pris": "85",
+        "kategori": "vegetar"
+    }, {
+        "navn": "Saag Paneer",
+        "billede": "saag-paneer",
+        "kort": "Hytteost med spinat.",
+        "lang": "",
+        "pris": "85",
+        "kategori": "vegetar"
+    }, {
+        "navn": "Chana Masala",
+        "billede": "chana-masala",
+        "kort": "Kikærter tilberedt i vores krydret karrysauce.",
+        "lang": "",
+        "pris": "85",
+        "kategori": "vegetar"
+    }, {
+        "navn": "Ris",
+        "billede": "rice",
+        "kort": "Ekstra ris.",
+        "lang": "",
+        "pris": "29",
+        "kategori": "ekstra"
+    }, {
+        "navn": "Naan brød",
+        "billede": "naan",
+        "kort": "Tandooribrød med gær, smør, æg og hvedemel.",
+        "lang": "",
+        "pris": "30",
+        "kategori": "ekstra"
+    }, {
+        "navn": "Mango Chutney",
+        "billede": "mango-chutney",
+        "kort": "Sød mango chutney.",
+        "lang": "",
+        "pris": "15",
+        "kategori": "ekstra"
+    }
+]
